eae95d1308aa498290aef2e363b799d20ac8e201
andresiglesias1996 <andresiglesias1996@gmail.com>
2024-04-23 22:16:23 -0300

    Se agrega  projectId al a configuracion inicial de cypress

---
+++ cypress.config.js
@@ -0,0 +1,11 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  //Ingresar el Project Id cloud.cypress 
+  projectId: process.env.CYPRESS_PROJECT_ID,
+  e2e: {
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
