0e088a158eddc98784e24f65471128809e61c426
andresiglesias1996 <andresiglesias1996@gmail.com>
2024-04-23 23:57:51 -0300

    Fix: soporte para variables de entorno con dotenv Configuración de GitHub Actions en cypress.yml

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,11 @@
+require('dotenv').config();
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   //Ingresar el Project Id cloud.cypress 
   projectId: process.env.CYPRESS_PROJECT_ID,
+
+    
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
