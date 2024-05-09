8b5b85c7c6c471c40097cc38eafa90f9501e7965
andresiglesias1996 <andresiglesias1996@gmail.com>
2024-05-09 10:12:32 -0300

    Actualizo capturas. Se agrega paso para obtener capturas

--- cypress-image-diff.config.js
+++ cypress-image-diff.config.js
@@ -2,6 +2,7 @@
 const config = {
     failureThreshold: 0.05, // Permitiría una diferencia de hasta el 5%
     failureThresholdType: 'percent', // Indica que el umbral es un porcentaje
+     //dónde se guardan todas las capturas de pantalla
   };
   
   module.exports = config;
