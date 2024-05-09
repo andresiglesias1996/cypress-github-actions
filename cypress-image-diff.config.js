a98e3e69e84421849920a255ee437e49bc981989
andresiglesias1996 <andresiglesias1996@gmail.com>
2024-05-09 12:00:29 -0300

    Cambio de ROOT_DIR

--- cypress-image-diff.config.js
+++ cypress-image-diff.config.js
@@ -3,6 +3,10 @@ const config = {
     failureThreshold: 0.05, // Permitiría una diferencia de hasta el 5%
     failureThresholdType: 'percent', // Indica que el umbral es un porcentaje
      //dónde se guardan todas las capturas de pantalla
+    ROOT_DIR: 'screenshots',
+  SCREENSHOTS_DIR: 'visual-test'
+
+
   };
   
   module.exports = config;
