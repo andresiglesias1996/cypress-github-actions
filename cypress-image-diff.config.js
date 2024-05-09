2a6e5bdf0b3956c45649e380942eefdc0a326e28
andresiglesias1996 <andresiglesias1996@gmail.com>
2024-05-09 15:21:55 -0300

    Modifico ruta de cypress-image-diff

--- cypress-image-diff.config.js
+++ cypress-image-diff.config.js
@@ -3,10 +3,8 @@ const config = {
     failureThreshold: 0.05, // Permitiría una diferencia de hasta el 5%
     failureThresholdType: 'percent', // Indica que el umbral es un porcentaje
      //dónde se guardan todas las capturas de pantalla
-    ROOT_DIR: 'screenshots',
-  SCREENSHOTS_DIR: 'visual-test'
-
-
+    ROOT_DIR: 'cypress',
+  SCREENSHOTS_DIR: 'screenshots'
   };
   
   module.exports = config;
--- cypress.config.js
+++ cypress.config.js
@@ -7,15 +7,20 @@ const getCompareSnapshotsPlugin = require('cypress-image-diff-js/plugin');
 module.exports = defineConfig({
   //Ingresar el Project Id cloud.cypress 
   projectId: process.env.CYPRESS_PROJECT_ID,
- 
-  
+
+
   e2e: {
-   
+
     setupNodeEvents(on, config) {
       // complemento de diferenciación de imágenes 
       return getCompareSnapshotsPlugin(on, config);
     },
+
     viewportHeight: 800,
-viewportWidth: 1200,  
+    viewportWidth: 1200,
   },
+  "screenshotOnRunFailure": true,
+  "video": true,
 });
+
+
