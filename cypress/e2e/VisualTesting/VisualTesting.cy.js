8eb0c530ea42a49458f5e42045fb8553d63c74c0
andresiglesias1996 <andresiglesias1996@gmail.com>
2024-05-09 17:15:09 -0300

    Se agrega espera en las capturas de pantalla

--- cypress/e2e/VisualTesting/VisualTesting.cy.js
+++ cypress/e2e/VisualTesting/VisualTesting.cy.js
@@ -2,12 +2,16 @@ describe('Test Ejemplo', () => {
   it('Visual Testing', () => {
     cy.visit('https://www.google.com');
 
+    cy.wait(2000);
     // Intenta capturar una imagen de la página completa y compararla con un snapshot previamente guardado con el nombre 'home-page'
     cy.compareSnapshot('home-page');
+    
   });
 
   it('Visual Testing con selector', () => {
     cy.visit('https://www.google.com');
+    cy.wait(2000);
+
 
     // Intenta seleccionar el formulario de búsqueda de Google y capturar una imagen de ese elemento específico,
     // luego compararla con un snapshot previamente guardado con el nombre 'google-search-form'
