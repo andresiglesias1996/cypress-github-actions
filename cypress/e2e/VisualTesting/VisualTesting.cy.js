9665dfecddb2f62359566c93df80a912a02cacbf
andresiglesias1996 <andresiglesias1996@gmail.com>
2024-05-09 18:04:47 -0300

    modifico  test e imagenes base

--- cypress/e2e/VisualTesting/VisualTesting.cy.js
+++ cypress/e2e/VisualTesting/VisualTesting.cy.js
@@ -1,20 +1,20 @@
 describe('Test Ejemplo', () => {
-  it('Visual Testing con selector 1', () => {
-    cy.visit('https://www.genexus.com/es/productos/genexus-enterprise-ai');
+  it('Visual Testing con selector1', () => {
+    cy.visit('https://www.linkedin.com/');
 
     cy.wait(2000);
     // Intenta capturar una imagen de la página completa y compararla con un snapshot previamente guardado con el nombre 'home-page'
-    cy.compareSnapshot('home-body > main > div.sector.I135912');
+    cy.get('.nav').compareSnapshot('selector1');
     
   });
 
   it('Visual Testing con selector 2', () => {
-    cy.visit('https://www.google.com');
+    cy.visit('https://www.linkedin.com/');
     cy.wait(2000);
 
 
     // Intenta seleccionar el formulario de búsqueda de Google y capturar una imagen de ese elemento específico,
     // luego compararla con un snapshot previamente guardado con el nombre 'google-search-form'
-    cy.get('form').compareSnapshot('body > main > div.sector.I135912 > div > div > section.region.I135893');
+    cy.get(':nth-child(1) > .flex-col > .input-label').compareSnapshot('selector2');
   });
 });
